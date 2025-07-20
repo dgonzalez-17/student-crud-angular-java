d2eafa9866bdad6ac97e5c0c13ab0baa6a04f845
Yesid Gonzalez <112140738+dgonzalez-17@users.noreply.github.com>
2025-07-20 14:21:25 -0500

    refactor: move backend dependencies to backend folder and frontend dependencies to frontend folder.

---
+++ frontend/ngx-admin/src/app/models/materias.model.ts
@@ -0,0 +1,13 @@
+export class Materias {
+    id?:string;
+    credits?:string;
+    title?:string;
+
+    constructor(id?:string, credits?:string, title?:string){
+        this.id = id;
+        this.credits = credits;
+        this.title = title;
+    }
+
+}
+
---
+++ frontend/ngx-admin/src/app/pages/alumno/asignate/asignate.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Alumno } from '../../../models/alumno.model';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AsignacionesService } from '../../../services/asignaciones.service';
+import { MateriasService } from '../../../services/materias.service';
+import { Materias } from '../../../models/materias.model';
+
+@Component({
+  selector: 'alumn-asignate',
+  templateUrl: './asignate.component.html',
+  styleUrls: ['./asignate.component.scss']
+})
+export class AsignateComponent implements OnInit {
+    isReadOnly = true;
+    name:  string;
+    document: string;
+    birthday: string;
+    career: string;
+    materia: string;
+  alertaActiva = false;
+  materias: Materias[] = []
+  public id: string = this.router.getCurrentNavigation().extras.state.id
+  public alumno: Alumno = this.router.getCurrentNavigation().extras.state.alumno
+
+
+  constructor( private asginacionesService: AsignacionesService, private materiasService: MateriasService, private router: Router ) { }
+  
+  consultarMaterias():void{
+    this.materiasService.consultarMaterias().subscribe(materiasConsulta => {
+      this.materias = materiasConsulta
+      console.log(this.materias)
+    });
+  }
+  
+
+  asignarMateria(alumnoId: string, materiaElegida: string){
+  this.asginacionesService.asignarMaterias(alumnoId, materiaElegida).subscribe(data => {
+    Swal.fire(
+      'Asignada!',
+      'La materia ha sido asignada correctamente.',
+      'success'
+    ),
+    this.router.navigate(['pages/alumno/read/']);
+    console.log(this.alumno.id);
+  },
+  error=>{
+    Swal.fire({
+      title: 'Error, no se pudo asignar la materia',
+      text: error["error"]["message"],
+      icon:"error",
+      timer:3000
+    });
+    this.alertaActiva = true;
+  }
+  );
+  
+  }
+
+  ngOnInit(): void {
+    this.consultarMaterias()
+  }
+
+}
+
